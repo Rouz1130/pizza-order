507f612eb7ab0bc46b9b4b85ce944caefadf94fb
Rouz Majlessi <rmajlessi@gmail.com>
2016-07-04 17:46:51 -0700

    changed the funcion name in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
-fucntion Pizza(size,speciality,quantity){
-this.size=size;
-this.toppings=speciality;
-this.quantity=quantity //no more than 4 orders
-this.price=price 
+fucntion PizzaOrder(speciality){
+  this.speciality = speciality;
+  this.quantity  = [0]
 }
+
+funtion quantity(one,two,three,four){
+  this.one = one;
+  this.two = two;
+  this.three = three;
+  this.four = four;
+}
+
+PizzaOrder.prototype.pizzaOrder = funtion() {
+  var = pizzaCost = 0;
+  if (ths.one === "small") {
+    price += 10;
+ } else if (this.two === "meduim"){
+    price += 15;
+ } else if (this.three === "Large"){
+    price += 20;
+ }
+if (this.quantity.length === 1){
+  if(this.quantity[0].small == )
+  prie += 10;
+}
+  if (this.quantity[1].meduim === )
